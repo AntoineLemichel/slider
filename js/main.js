523f4347aa2f6544eee289de9645dd58fadc35d1
AntoineLemichek <antoine.lemichel@newgates.fr>
2018-08-29 15:16:56 +0200

    Bug fix to spam with arrow is now fixed

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ let image = document.getElementById("image-slide");
 let imageArray = ["img/boxer.jpg", "img/fitness.jpg", "img/jogging.jpg"];
 let index = 0;
 const INDEXMAX = imageArray.length - 1;
-let timer = setInterval(sliderAuto, 3000);
 let text = document.getElementById("text");
 text.innerHTML = "Picture's Boxer";
 
@@ -36,24 +35,24 @@ function sliderAuto() {
 
 //This function, manage left arrow click.
 document.getElementById("leftArrow").addEventListener("click", function () {
+  clearInterval(timer);
   //If index equal at 0, so define index to INDEXMXA. Else index--
   if (index == 0) {
     index = INDEXMAX;
   } else {
     index--;
   }
-
   //Change src img into HTML, with array and index.
   image.src = imageArray[index];
   //Stop the timer for any click on arrow.
-  clearInterval(timer);
+  timer = setInterval(sliderAuto, 3000);
   //Restart the timer after 3sec.
-  setInterval(sliderAuto, 3000);
 });
 
 
 //This function, manage the right arrow click.
 document.getElementById("rightArrow").addEventListener("click", function () {
+  clearInterval(timer);
   //If index superior or equal INDEXMAX, so index define 0. Else increments index.
   if (index >= INDEXMAX) {
     index = 0;
@@ -64,8 +63,9 @@ document.getElementById("rightArrow").addEventListener("click", function () {
   image.src = imageArray[index];
 
   //Stop the timer.
-  clearInterval(timer);
 
   //Restart the timer after 3sec.
-  setInterval(sliderAuto, 3000);
+  timer = setInterval(sliderAuto, 3000);
 });
+
+let timer = setInterval(sliderAuto, 3000);
